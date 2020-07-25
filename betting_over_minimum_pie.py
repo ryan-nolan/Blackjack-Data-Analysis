b794147acd09014ee5b6612fc5771892080cd657
zrnx <nolanr6969@gmail.com>
2020-07-25 23:25:36 +0100

    Added files to track player stakes

---
+++ betting_over_minimum_pie.py
@@ -0,0 +1,107 @@
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import csv
+import os
+from pathlib import Path
+from collections import Counter
+import statistics
+import sys
+import operator
+
+def autopct(values):
+    def my_autopct(pct):
+        sumOf = sum(values)
+        ret = int(round(pct * sumOf / 100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=ret)
+    return my_autopct
+
+# %%
+#File Handling
+path = Path("C:/Users/Ryan/Desktop/Dissertation/Data") #insert folder path here
+os.chdir(path)
+files = os.listdir()
+print(sys.argv)
+
+# %%
+if len(sys.argv) == 1: 
+    for file in files:
+        if os.path.isfile(file):
+            with open(file) as csvfile:
+                csv_reader = csv.reader(csvfile)
+                print(file)
+                overMinBetPercentage = {}
+                i = 0
+                for row in csv_reader:
+                    turnNumber = row[0]
+                    stake = row[8]
+                    if i == 2:
+                        minBet = stake
+                    
+                    overMinBetPercentage[turnNumber] = stake
+                    i+= 1
+                overMinBetCounter = Counter()
+                for result in overMinBetPercentage.values():
+                    overMinBetCounter[result] += 1
+                del overMinBetCounter['PlayerStake']
+                
+                print(overMinBetCounter)
+                minBetString = overMinBetCounter.most_common(1)[0][0]
+                print(minBetString)
+                sortedOverMinBetFreq = {'MinBet' : 0, 'Above MinBet': 0}
+                for key in overMinBetCounter:
+                    if key == minBetString:
+                        sortedOverMinBetFreq['MinBet'] += overMinBetCounter[key]
+                    else:
+                        sortedOverMinBetFreq['Above MinBet'] += overMinBetCounter[key]
+                print(sortedOverMinBetFreq)
+
+                
+                figureObject, axesObject = plt.subplots()
+                axesObject.pie(sortedOverMinBetFreq.values(), labels=sortedOverMinBetFreq.keys(), autopct=autopct(sortedOverMinBetFreq.values()),startangle=90)
+
+                plt.title(file+ "\nMinimum Bet Percentage")
+                plt.savefig(os.getcwd()+'\\Graphs\\AboveMinBetPie\\'+file+'.png')
+                
+                plt.show()
+"""
+elif len(sys.argv) == 2:
+    file = sys.argv[1]
+    if os.path.isfile(file):
+        with open(file) as csvfile:
+            csv_reader = csv.reader(csvfile)
+            print(file)
+            winPercentage = {}
+            for row in csv_reader:
+                turnNumber = row[0]
+                gameResult = row[4]
+                splitGameResult = row[5]
+
+                winPercentage[turnNumber] = gameResult
+            winPercentageCounter = Counter()
+            for result in winPercentage.values():
+                winPercentageCounter[result] += 1
+            
+            sortedWinPercentageFreq = {'W': 0,'L': 0,'D': 0}
+            for key in winPercentageCounter:
+                if key == 'W':
+                    sortedWinPercentageFreq['W'] = winPercentageCounter[key]
+                if key == 'L':
+                    sortedWinPercentageFreq['L'] = winPercentageCounter[key]
+                if key == 'D':
+                    sortedWinPercentageFreq['D'] = winPercentageCounter[key]
+                if key == 'W_N':
+                    sortedWinPercentageFreq['W'] += winPercentageCounter[key]
+                if key == 'L_N':
+                    sortedWinPercentageFreq['L'] += winPercentageCounter[key]
+                if key == 'D_N':
+                    sortedWinPercentageFreq['D'] += winPercentageCounter[key]
+            print(sortedWinPercentageFreq)
+            figureObject, axesObject = plt.subplots()
+            axesObject.pie(sortedWinPercentageFreq.values(), labels=sortedWinPercentageFreq.keys(), autopct=autopct(sortedWinPercentageFreq.values()),startangle=90)
+
+            plt.title(file+ "\nWin Percentage")
+            plt.savefig(os.getcwd()+'\\Graphs\\WinPercentagePieNoNaturals\\'+file+'.png')
+            plt.show()
+"""
---
+++ exact_bet_amount_pie.py
@@ -0,0 +1,69 @@
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import csv
+import os
+from pathlib import Path
+from collections import Counter
+import statistics
+import sys
+import operator
+
+def autopct(values):
+    def my_autopct(pct):
+        sumOf = sum(values)
+        ret = int(round(pct * sumOf / 100.0))
+        return '{p:.2f}%  ({v:d})'.format(p=pct,v=ret)
+    return my_autopct
+
+# %%
+#File Handling
+path = Path("C:/Users/Ryan/Desktop/Dissertation/Data") #insert folder path here
+os.chdir(path)
+files = os.listdir()
+print(sys.argv)
+
+# %%
+if len(sys.argv) == 1: 
+    for file in files:
+        if os.path.isfile(file):
+            with open(file) as csvfile:
+                csv_reader = csv.reader(csvfile)
+                print(file)
+                overMinBetPercentage = {}
+                i = 0
+                for row in csv_reader:
+                    turnNumber = row[0]
+                    stake = row[8]
+                    if i == 2:
+                        minBet = stake
+                    
+                    overMinBetPercentage[turnNumber] = stake
+                    i+= 1
+                overMinBetCounter = Counter()
+                for result in overMinBetPercentage.values():
+                    overMinBetCounter[result] += 1
+                del overMinBetCounter['PlayerStake']
+                
+                print(overMinBetCounter)
+                """
+                minBetString = overMinBetCounter.most_common(1)[0][0]
+                print(minBetString)
+                sortedOverMinBetFreq = {'MinBet' : 0, 'Above MinBet': 0}
+                for key in overMinBetCounter:
+                    if key == minBetString:
+                        sortedOverMinBetFreq['MinBet'] += overMinBetCounter[key]
+                    else:
+                        sortedOverMinBetFreq['Above MinBet'] += overMinBetCounter[key]
+                print(sortedOverMinBetFreq)
+                """
+                
+                figureObject, axesObject = plt.subplots()
+                axesObject.pie(overMinBetCounter.values(), labels=overMinBetCounter.keys(), autopct=autopct(overMinBetCounter.values()),startangle=90)
+
+                plt.title(file+ "\nAmount Bet Percentage")
+                plt.savefig(os.getcwd()+'\\Graphs\\ExactBetAmountPie\\'+file+'.png')
+                
+                plt.show()
+                
